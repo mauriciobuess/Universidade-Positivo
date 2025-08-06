b3ea46a3999ee1f203f38a444e964124363019f9
Maurício Buess <mauriciobuess@gmail.com>
2025-08-05 23:22:42 -0300

    Delete 2024 directory

--- 2024/Desenvolvimento_Aplicativos_Moveis/Aulas/IntelliJ/Aula03_Exercicio02/src/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    print("Digite uma frase: ")
-    val frase = readLine()!!
-
-    val palavras = frase.split(" ")
-    val numeroPalavras = palavras.size
-
-    println("Número de palavras na frase: $numeroPalavras")
-}
